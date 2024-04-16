c714bb506d02865da46aace0ffcea6d92e1381fc
VladislavBogdanov <166297638+VladislavBogdanov@users.noreply.github.com>
2024-04-16 19:08:32 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list=['яблоко', 'груша', 'банан', 'киви', 'мандарин', 'арбуз', 'вишня']
+print(my_list)
+first_fruit=my_list[0] # Можно было и просто индексами!
+last_fruit=my_list[-1]
+print(first_fruit)
+print(last_fruit)
+print(my_list[2:4])
+my_list[2]='виноград'
+print(my_list)
+
+my_dict={'дом':'house','комната': 'rum', 'квартира': 'flat' }
+print(my_dict['комната'])
+my_dict['комната'] = 'room'
+print(my_dict)
